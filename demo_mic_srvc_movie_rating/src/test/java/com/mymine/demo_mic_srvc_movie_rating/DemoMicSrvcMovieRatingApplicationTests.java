743ff50761c52f963dcfa85cf9f84f69ce4ef303
m.m.nabel@gmail.com <LowLow@1WaWa>
2020-12-11 10:13:22 -0500

    movie rating microservice added

---
+++ demo_mic_srvc_movie_rating/src/test/java/com/mymine/demo_mic_srvc_movie_rating/DemoMicSrvcMovieRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymine.demo_mic_srvc_movie_rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicSrvcMovieRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
