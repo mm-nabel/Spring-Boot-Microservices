6f49011b24a2d82fa445a81352a172176d7a33cd
m.m.nabel@gmail.com <LowLow@1WaWa>
2020-12-11 09:41:30 -0500

    adding the movie catalog microservice to the project

---
+++ demo_mic_srvc_movie_catalog/src/test/java/com/mymine/demo_mic_srvc_movie_catalog/DemoMicSrvcMovieCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymine.demo_mic_srvc_movie_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicSrvcMovieCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
