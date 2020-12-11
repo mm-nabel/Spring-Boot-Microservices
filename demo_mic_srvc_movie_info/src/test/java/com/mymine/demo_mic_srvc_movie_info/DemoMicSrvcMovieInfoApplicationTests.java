6286c5e608b44d7935c554172bfd5cf11ffdebbe
m.m.nabel@gmail.com <LowLow@1WaWa>
2020-12-11 10:49:37 -0500

    Movie Catalog,Information,Rating Microservices added

---
+++ demo_mic_srvc_movie_info/src/test/java/com/mymine/demo_mic_srvc_movie_info/DemoMicSrvcMovieInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymine.demo_mic_srvc_movie_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicSrvcMovieInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ demo_mic_srvc_movie_rating/src/main/java/com/mymine/demo_mic_srvc_movie_rating/DemoMicSrvcMovieRatingApplication.java
@@ -0,0 +1,13 @@
+package com.mymine.demo_mic_srvc_movie_rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicSrvcMovieRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicSrvcMovieRatingApplication.class, args);
+	}
+
+}
